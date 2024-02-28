36534182a748581bd9903f312171aa59d3a2c835
Sönke Liebau <soenke.liebau@stackable.tech>
2024-02-28 10:01:08 +0100

    Initial commit

---
+++ stack_scanner/main.py
@@ -0,0 +1,54 @@
+from urllib.request import urlretrieve
+from image_tools.args import load_configuration
+import tempfile
+import os
+
+releases = [
+    '23.4.0',
+    '23.4.1',
+    '23.7.0',
+    '23.11.0',
+]
+
+excluded_products = [
+    'hello-world',
+    'java-base',
+]
+
+REGISTRY_URL = "docker.stackable.tech"
+
+
+def main():
+    with tempfile.TemporaryDirectory() as tempdir:
+        for release in releases:
+            # Create a file in the temp dir and download the conf.py from the git tag referring to that version
+            filename = os.path.join(tempdir, f"products-{release}.py")
+            url = f"https://raw.githubusercontent.com/stackabletech/docker-images/{release}/conf.py"
+            oldurl = f"https://raw.githubusercontent.com/stackabletech/docker-images/{release}/image_tools/conf.py"
+            print(f"Loading product config for version [{release}] from [{url}] (via file [{filename}]")
+            try:
+                urlretrieve(url, filename)
+            except:
+                print(f"Got 404 for release file, falling back to old file location [{oldurl}]")
+                try:
+                    urlretrieve(oldurl, filename)
+                except:
+                    print(f"Unable to retrieve config file for release [{release}], skipping ..")
+                    continue
+
+            # Load product versions from that file using the image-tools functionality
+            product_versions = load_configuration(filename)
+
+            # Generate image names
+            product_names: list[str] = [product["name"] for product in product_versions.products]
+            for product in product_versions.products:
+                product_name: str = product["name"]
+                if product_name in excluded_products:
+                    continue
+                product_targets = {}
+                for version_dict in product.get("versions", []):
+                    print(f"{REGISTRY_URL}/stackable/{product_name}:{version_dict['product']}-stackable{release}")
+
+
+
+if __name__ == "__main__": main()
